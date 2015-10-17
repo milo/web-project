8deb7c6116f5338a1f5e451ce3486b81c1ef3231
Miloslav Hůla <miloslav.hula@gmail.com>
2015-10-18 00:24:10 +0200

    bootstrap.php: tweaks

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,20 +1,50 @@
 <?php
 
+function dump($var)
+{
+	Tracy\Dumper::dump($var, [
+		Tracy\Dumper::TRUNCATE => 1024,
+		Tracy\Dumper::COLLAPSE => 30,
+		Tracy\Dumper::COLLAPSE_COUNT => 15,
+		Tracy\Dumper::DEPTH => 6,
+	]);
+
+	return $var;
+}
+
+function bdump($var)
+{
+	Tracy\Debugger::barDump($var);
+	return $var;
+}
+
 require __DIR__ . '/../vendor/autoload.php';
+
+Tracy\Debugger::$maxDepth = 10;
 
 $configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
-$configurator->enableDebugger(__DIR__ . '/../log');
+if (PHP_SAPI === 'cli') {
+	$configurator->setDebugMode(!in_array('--production', $_SERVER['argv'], TRUE));
+} else {
+	$configurator->setDebugMode([
+	]);
+}
 
+$configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
 $configurator->createRobotLoader()
 	->addDirectory(__DIR__)
+	->addDirectory(__DIR__ . '/../lib')
 	->register();
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
+if ($configurator->isDebugMode()) {
+#	Dibi\Bridges\Tracy\Panel::$maxLength = 65535;
+	$configurator->addConfig(__DIR__ . '/config/config.debug.neon');
+}
 
 $container = $configurator->createContainer();
 
